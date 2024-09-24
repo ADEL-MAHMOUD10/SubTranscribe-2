448ee7d554181cbe118299f39b8e801ec89c5c06
A D E L M A H M O U D <adelam578568@gmail.com>
2024-09-25 02:18:58 +0300

    Update script.js

--- static/js/script.js
+++ static/js/script.js
@@ -18,7 +18,7 @@ setInterval(() => {
         .catch(error => {
             console.error('Error fetching progress:', error);
         });
-}, 5000);
+}, 11000);
 
 // Display selected file name dynamically
 function showFileName() {
