967b8056207fcb46cf208501f1a0450e89908bcd
A D E L M A H M O U D <adelam578568@gmail.com>
2024-10-02 20:04:07 +0300

    Update app.py

--- app.py
+++ app.py
@@ -231,4 +231,4 @@ def serve_file(filename):
 if __name__ == '__main__':
     if not os.path.exists(app.config['UPLOAD_FOLDER']):
         os.makedirs(app.config['UPLOAD_FOLDER'])
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 8000)))
