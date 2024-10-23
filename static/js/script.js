6a2dd17e567d8b26800de9408d974c75c7a27ff1
A D E L M A H M O U D <adelam578568@gmail.com>
2024-10-23 23:02:43 +0300

    Update script.js

--- static/js/script.js
+++ static/js/script.js
@@ -14,7 +14,11 @@ window.addEventListener('DOMContentLoaded', (event) => {
 });
 
 function resetProgressStatus() {
-    fetch('/reset-progress', { method: 'POST' })
+    fetch('/reset-progress', { method: 'POST',
+    credentials: 'include',
+    headers: {
+        'Content-Type': 'application/json'
+    }})
         .then(response => {
             if (!response.ok) {
                 throw new Error('Network response was not ok.');
