7238ffedec814dcd2b8fa47bc94ccc25d368f0f6
A D E L M A H M O U D <adelam578568@gmail.com>
2024-10-27 23:11:22 +0300

    Update script.js

--- static/js/script.js
+++ static/js/script.js
@@ -14,7 +14,7 @@ window.addEventListener('DOMContentLoaded', (event) => {
 });
 
 function resetProgressStatus() {
-    fetch('/reset-progress', { method: 'GET' })
+    fetch('https://subtranscribe.koyeb.app/reset-progress', { method: 'GET' })
         .then(response => {
             if (!response.ok) {
                 throw new Error('Network response was not ok.');
@@ -31,7 +31,7 @@ function resetProgressStatus() {
 
 // Continue with your interval function
 const intervalId = setInterval(async function () {
-    await fetch('/progress', {
+    await fetch('https://subtranscribe.koyeb.app/progress', {
         method: 'GET',
         credentials: 'include',
         headers: {
