3a347fbb1c44d20bc50ac84f25a491909fa41013
A D E L M A H M O U D <adelam578568@gmail.com>
2024-11-01 14:45:04 +0200

    Update script.js

--- static/js/script.js
+++ static/js/script.js
@@ -76,7 +76,7 @@ const intervalId = setInterval(function () {
             console.error('Error fetching progress:', error);
             document.getElementById('progressMessage').innerText = "Error fetching progress. Please try again.";
         });
-    }, 1000); // Poll every 1 seconds
+    }, 2000); // Poll every 2 seconds
 
 // Display selected file name dynamically
 function showFileName() {
